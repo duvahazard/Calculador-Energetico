ee681715572d2e94c537716492d821d02701c63d
duvahazard <duvahazard@gmail.com>
2011-08-24 12:27:34 -0700

    Le puse un comentario de prueba

--- caminoSolar.py
+++ caminoSolar.py
@@ -45,7 +45,7 @@ num=int(num)
 longitude = 0
 latitude = 0
 
-id_terreno = '32' #id temporal del terreno a leer
+id_terreno = '32' #id temporal del terreno a leer para calculos
 
 #aqui vamos a generar la tabla para el numero de terreno
 cursor.execute("""DROP TABLE IF EXISTS ce_camino_solar_%st""" % (id_terreno))
