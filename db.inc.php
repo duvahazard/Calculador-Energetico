20070dba15819598ae936a7aaa3ba5815aa6ddfe
Minoru Nomura <kaos_93@hotmail.com>
2011-11-18 15:33:04 -0800

    se borraron estos archivos

--- db.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Conexion a base de datos 
-//$con=mysql_connect("158.97.19.235", "rodger", "comp4510n"); // para Rodger
-$con=mysql_connect("127.0.0.1", "root", "");
-if (!$con) {
-  die('No se pudo conectar: ' . mysql_error());
-}
-mysql_select_db("calculador", $con);
-?>
