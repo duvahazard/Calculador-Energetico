b7a8d7a4e950523d0dc2dcf4eb4340a40e2a1745
Minoru Nomura <kaos_93@hotmail.com>
2011-11-19 13:28:32 -0800

    variables para version local y en el server.

--- webpage/conexion.php
+++ webpage/conexion.php
@@ -1,9 +1,11 @@
 <?php
 $conn = mysql_connect("localhost", "root","" );
+//$conn = mysql_connect("localhost", "voxelsol_calculador","c4lcul4d0r!" );
 if(!$conn){
 	die('Error en la conexi&oacute;n a la base de datos, consulte a su administrador...');
 }
 mysql_select_db("calculador", $conn);
+//mysql_select_db("voxelsol_calculador", $conn);
 			
 
 ?>
