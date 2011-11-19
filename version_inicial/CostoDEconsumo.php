b0d1d6468f06989ce20d6f5d7058cfd27f7a7042
sunnycanuck <sunnycanuck@gmail.com>
2011-11-19 13:39:28 -0800

    costo

---
+++ version_inicial/CostoDEconsumo.php
@@ -0,0 +1,29 @@
+<?php    
+/* A program to calculate value of a CEF repipt for a given tarrif and energy consumption in a month for the calculado_energetico.
+ 
+  Project Leader Rodger Evans, 2011-11-09
+  sunnycanuck@gmail.com
+  LEARS de CICESE
+  revans@cicese.mx
+  Collaborators Voxel Soluciones
+  http://www.voxelsoluciones.com
+  info@voxelsoluciones.com
+
+  Published under the Creative Commons Attribution-ShareAlike 2.5 Generic (CC BY-SA 2.5) licence
+  http://creativecommons.org/licenses/by-sa/2.5/
+
+  Publicado bajo la Licencia Creative Commons Atribuci√≥n-CompartirIgual 2.5 M√©xico (CC BY-SA 2.5) 
+  http://creativecommons.org/licenses/by-sa/2.5/mx/
+  
+*/        
+//cycle through casos or be called for a particular case
+
+if (){ //if ce_medidorCFE_YYcaso does not exist then run medidor.php	
+}
+//cycle through the entries of ce_medidorCFE_YYcaso
+//take the difference between one row and the next, use as the energy used that month (if bimesual the do this for two months)
+
+//is the month is in the summer or winter
+
+
+?>
---
+++ version_inicial/medidor.php
@@ -0,0 +1,28 @@
+<?php    
+/* A program to calculate the simulated reading on a CFE meter for the calculado_energetico.
+ 
+  Project Leader Rodger Evans, 2011-11-09
+  sunnycanuck@gmail.com
+  LEARS de CICESE
+  revans@cicese.mx
+  Collaborators Voxel Soluciones
+  http://www.voxelsoluciones.com
+  info@voxelsoluciones.com
+
+  Published under the Creative Commons Attribution-ShareAlike 2.5 Generic (CC BY-SA 2.5) licence
+  http://creativecommons.org/licenses/by-sa/2.5/
+
+  Publicado bajo la Licencia Creative Commons Atribuci√≥n-CompartirIgual 2.5 M√©xico (CC BY-SA 2.5) 
+  http://creativecommons.org/licenses/by-sa/2.5/mx/
+  
+*/
+
+//reads ce_consumo, ce_Casos
+
+//make a table called ce_medidorCFE_01caso that is the same format in number of rows as ce_HorasDelMes with ce_medidorCFE_01caso:fetcha taken from año y mes
+
+//read the values for ce_consumo for tipo=3, if non exists then generate one (if more then one exists, delet both and make a new one).
+
+//To make 
+
+?>
