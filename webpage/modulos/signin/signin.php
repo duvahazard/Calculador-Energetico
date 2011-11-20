3b8cd9ef942de2cfdd50385eca646612ce833ce7
Minoru Nomura <kaos_93@hotmail.com>
2011-11-19 18:19:38 -0800

    se agrego opcion de registro para proveedores.

--- webpage/modulos/signin/signin.php
+++ webpage/modulos/signin/signin.php
@@ -1,8 +1,63 @@
 <div class="signin">
-	<h2>Registrarse</h2>
-  <p>Favor de llenar los siguientes campos, todos son obligatorios.</p>
+	<?php if($_REQUEST['tipo_usuario'] == 2){ ?>
+  <h4>Proveedor</h4>
+  <p style="margin-bottom:0;">Favor de llenar los siguientes campos, todos son obligatorios.</p>
+  <cite style="margin-bottom:20px; display:block;">Nota: El campo "usuario" es su correo electr&oacute;nico</cite>
   <div id="signin_table">
   	<form action="sql.php?mod=2&act=1" method="post" target="_parent" class="AdvancedForm">
+    <input name="tipo_usuario" type="hidden" value="<?php echo $_REQUEST['tipo_usuario']; ?>" />
+    <table cellpadding="0" cellspacing="0" border="0" width="80%">
+    	<tr>
+      	<td width="31%">Nombre completo:</td>
+        <td width="69%"><input type="text" name="name" class="general" id="name"/></td>
+      </tr>
+      <tr>
+      	<td>Usuario:</td>
+        <td><input type="text" name="user" class="general" id="user"/></td>
+      </tr>
+      <tr>
+      	<td>Contrase&ntilde;a:</td>
+        <td><input type="password" name="pass" id="pass" class="general"/></td>
+      </tr>
+      <tr>
+      	<td>Repetir contrase&ntilde;a:</td>
+        <td><input type="password" name="confirmpass" id="confirmpass" class="general"/></td>
+      </tr>
+      <tr>
+      	<td>Direcci&oacute;n:</td>
+        <td><input type="text" name="direccion" id="direccion" class="general"/></td>
+      </tr>
+      <tr>
+      	<td>R.F.C:</td>
+        <td><input type="text" name="rfc" id="rfc" class="general"/></td>
+      </tr>
+      <tr>
+      	<td>Tel&eacute;fono:</td>
+        <td><input type="text" name="tel" id="tel" class="general"/></td>
+      </tr>
+      <tr>
+      	<td>Fax:</td>
+        <td><input type="text" name="fax" id="fax" class="general"/></td>
+      </tr>
+      <tr>
+      	<td>URL:</td>
+        <td><input type="text" name="url" id="url" class="general"/></td>
+      </tr>
+      <tr>
+      	<td>Ciudad:</td>
+        <td><input type="text" name="ciudad" id="ciudad" class="general"/></td>
+      </tr>
+    </table>    
+  </div><!-- tabla login -->
+  <div id="login_boton">
+  	<input type="image" src="images/signin.png" class="signin_btn" border="0" />
+  </div><!-- login_boton -->
+  </form>
+  <?php }elseif($_REQUEST['tipo_usuario'] == 3){ ?>
+		<p>Favor de llenar los siguientes campos, todos son obligatorios.</p>
+  <div id="signin_table">
+  	<form action="sql.php?mod=2&act=1" method="post" target="_parent" class="AdvancedForm">
+    <input name="tipo_usuario" type="hidden" value="<?php echo $_REQUEST['tipo_usuario']; ?>" />
     <table cellpadding="0" cellspacing="0" border="0" width="80%">
     	<tr>
       	<td width="31%">Nombre completo:</td>
@@ -22,8 +77,22 @@
       </tr>
     </table>    
   </div><!-- tabla login -->
-  <div>
+  <div id="login_boton">
   	<input type="image" src="images/signin.png" class="signin_btn" border="0" />
   </div><!-- login_boton -->
   </form>
+	<?php }else{ ?>
+  <h2>Registrarse</h2>
+  
+  <p>Seleccion el tipo de usuario</p>
+  <div id="tipo_usuario">
+  <form action="actions.php?id=2" method="post" class="AdvancedForm">
+  <p style="text-align:left; margin-bottom:0;"><input name="tipo_usuario" type="radio" value="3" />Usuario Normal</p>
+  <p style="text-align:left;"><input name="tipo_usuario" type="radio" value="2" /> Proveedor</p>
+  <div id="login_boton">
+  	<input type="image" src="images/signin.png" class="signin_btn" border="0" />
+  </div><!-- login_boton -->
+  </form>
+  </div>
+  <?php } ?>
 </div>
--- webpage/modulos/signin/sql.php
+++ webpage/modulos/signin/sql.php
@@ -2,19 +2,59 @@
 function query(){
 	
 	switch($_REQUEST['act']){
-		case 1:{
-			$name = $_REQUEST['name'];
-			$user = $_REQUEST['user'];
-			$pass = md5($_REQUEST['pass']);
-			
-			if($_REQUEST['pass'] === $_REQUEST['confirmpass']){
-				if(mysql_query("INSERT INTO ce_usuarios (nombre, usuario, pass, tipo) VALUES ('$name', '$user', '$pass', 0);")){
-					$url = "index.php?mod=1&id=1";
+		case 1:{			
+			$tipo = $_REQUEST['tipo_usuario'];
+			if($tipo == 2){
+				
+				$query = mysql_fetch_array(mysql_query("SELECT COUNT(id) AS total FROM ce_usuarios WHERE ce_usuarios.usuario = '$user';"));
+
+				$name = $_REQUEST['name'];
+				$user = $_REQUEST['user'];
+				$tel = $_REQUEST['tel'];
+				$url = $_REQUEST['ur'];
+				$pass = md5($_REQUEST['pass']);
+				$ciudad = $_REQUEST['ciudad'];
+				$direccion = $_REQUEST['direccion'];
+				$rfc = $_REQUEST['rfc'];
+				$fax = $_REQUEST['fax'];
+				
+				
+				if($query['total'] != 0){
+					if($_REQUEST['pass'] === $_REQUEST['confirmpass']){
+						if(mysql_query("INSERT INTO ce_usuarios (nombre, usuario, tel, url, pass, tipo, ciudad, direccion, rfc, fax, activado) VALUES ('$name', '$user', '$tel', '$url', '$pass', '$tipo', '$ciudad', '$direccion', '$rfc', '$fax', 0);")){
+							$url = "index.php?mod=1&ide=1";
+						}else{
+							$url = "index.php?mod=1&ide=2";
+						}
+					}else{
+						$url = "index.php?mod=1&ide=3";
+					}
 				}else{
-					$url = "index.php?mod=1&id=2";
+					$url = "index.php?mod=1&ide=6";
 				}
-			}else{
-				$url = "index.php?mod=1&id=3";
+				
+			}elseif($tipo == 3){
+				
+				$name = $_REQUEST['name'];
+				$user = $_REQUEST['user'];
+				$pass = md5($_REQUEST['pass']);
+				
+				$query = mysql_fetch_array(mysql_query("SELECT COUNT(id) AS total FROM ce_usuarios WHERE ce_usuarios.usuario = '$user';"));				
+				
+				if($query['total'] != 0){
+					if($_REQUEST['pass'] === $_REQUEST['confirmpass']){
+						if(mysql_query("INSERT INTO ce_usuarios (nombre, usuario, pass, tipo, activado) VALUES ('$name', '$user', '$pass', '$tipo', 0);")){
+							$url = "index.php?mod=1&ide=1";
+						}else{
+							$url = "index.php?mod=1&ide=4";
+						}
+					}else{
+						$url = "index.php?mod=1&ide=5";
+					}
+				}else{
+					$url = "index.php?mod=1&ide=6";
+				}
+				
 			}
 		}//CASE 1
 		;break;
