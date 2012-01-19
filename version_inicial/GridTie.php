d3c16cbc1dc1df2b5d859d05d9cc2c68aa4d801b
duvahazard <duvahazard@gmail.com>
2012-01-18 20:06:11 -0800

    Se comenzó a agregar el código de php y mysql para realmente trabajar con los datos capturados en la BD

--- version_inicial/GridTie.php
+++ version_inicial/GridTie.php
@@ -18,36 +18,53 @@
   
 */
 
-//una program para generar ce_gridTie_YYcaso
-$casoGridTie=;//in ce_Casos filter for gridTie (tipo=4)
+// Obtener con un Query sobre la tabla de casos solo los que sean de tipo 4 (gridTie)
+$casosGridTie = mysql_query(query);
+// Guardar en una variable el numero de registros de tipo 4 (gridTie) o hacer un while
 $casoLength=;//length of $casoGridTie
-$caso=1;
-while ($caso<=$casoLength) {  //cycle through each grid tie
-//make table ce_gridtie_XXc that has the same length and time stamps as ce_horasDelMes 
+
+// Empezar con el primer registro  estas operaciones
+while ($caso<=$casoLength) {  //ciclar en cada gridTie
+	// Hacer una tabla ce_gridtie_XXc que tenga la misma longitud y fecha/tiempo que ce_horasDelMes
+	// CREATE TABLE ce_gridtie_XXc
+	// SELECT fecha/tiempo FROM ce_horasDelMes
+	// INSERT fecha/tiempo a ce_gridtie_XXc
+		
+	// leer el factor de efectividad de tabla ce_dispositivo, el 2do factor que es porcentaje y guardarlo en una variable	
 	$eff=;//ce_dispositivo:factores(2) (%)
-	$mesTotal=0;//a vector to save the totals for each month, 1-12 for the months, 13 is a leap year where Feb has 29 days (this is done as cludge where the value from the 28th is just repeated.)
-	$fotoVolList=;//dispositivo_variables from gridTie
-	while(){//cycle through $fotoVolList
-		if(){//the fotovoltaics used are referenced in dispositivo_variables; does each reference have a sequence of their response? ce_fotovoltaico_respuesta_02c, if not, run FotovolRespuesta.php to generate it
-		}
-		$mes=1;//start with the first month in 
-		while($mes<=13){//cycle through the months in grid tie        
-			if($mes=13){//this is for the leap year, sum for feb, but with 28th counted twice
-				$mesList=;//filter the fotovol table to show just values for $mes=2
-				$mesTotal($mes)=$mesTotal+;//sum all the values in ce_fotorespuesta for that month, multiply by $eff and add to mes total.
-				}
-				else(){
-			$mesList=;//filter the fotovol table to show just values for $mes
-			$mesTotal($mes)=$mesTotal+;//sum all the values in ce_fotorespuesta for that month, multiply by $eff and add to mes total
-				}
-		}
-		$gridMes=1;//start in Jan
-    while($gridMes<=12){//cycle through all months and set the same production values for each.
-	$gridMesFilter=;//the grid tie filtered for months=$gridMes
 	
-	$gridMesFilter(produccion)=$mesTotal($gridMes);//set the production value to that from $mesTotal
-	if($gridMes=2){//leave in only leap years (2012,2016,2020,2024,2028,2032,2036,2040,2044,2048,2052,2056,2060,2064,2068,2072,2076,2080,2084,2088,2092,2096)
-		$gridMesFilter(produccion)=$mesTotal(13);
+	// ir guardando en arreglo de 13 lugares los totales de cada mes, del 1-12 y el 13 se guarda cuando sea leap year (biciesto) cuando Feb tenga 29 dias (simplemente en este caso se copia lo mismo del día 28)
+	array($mesTotal);
+
+	// Obtener de tabla de ce_Casos de la columna de dispositivo_variable la lista de los dispositivos FV que estan conectados a ese gridTie, estan separados por coma si hay mas de uno
+	// SELECT dispositivos_variable FROM ce_Casos
+	// explode con coma separados
+	array($fotoVolList);//dispositivo_variables from gridTie
+
+	// Con cada dispositivo FV hacer...
+	while(){
+		if(){ // revisar si cada dispositivo FV tiene su tabla de respuesta generada (ce_fotovoltaico_respuesta_02c) y si no ejecutar  FotovolRespuesta.php para generarlo
+		}
+
+		// empezar agarrando todos los meses 1 y sumar acumulando valores
+		$mes=1;//start with the first month in 
+		while($mes<=13){ // ciclar en los meses de grid tie
+			if($mes=13){ // caso cuando sea leap year, sumar febrero pero con el 28 contado 2 veces
+				$mesList=; // filtrar la tabla fotovol para mostrar solo valores de $mes=2
+				$mesTotal($mes)=$mesTotal+; // sumar todos los valores en ce_fotorespuesta para ese mes, multiplicarlo por $eff y sumarlo al mes total
+			}
+			else(){
+				$mesList=; // filter de la tabla fotovol para mostrar los demás meses
+				$mesTotal($mes)=$mesTotal+; // sumar todos los valores en ce_fotorespuesta para ese mes, multiplicarlo por $eff y sumarlo al mes total
+			}
+		}
+		$gridMes=1; // empezar en Enero
+		    while($gridMes<=12) { // cycle through all months and set the same production values for each.
+			$gridMesFilter=;//the grid tie filtered for months=$gridMes
+			
+			$gridMesFilter(produccion)=$mesTotal($gridMes);//set the production value to that from $mesTotal
+			if($gridMes=2){//leave in only leap years (2012,2016,2020,2024,2028,2032,2036,2040,2044,2048,2052,2056,2060,2064,2068,2072,2076,2080,2084,2088,2092,2096)
+				$gridMesFilter(produccion)=$mesTotal(13);
 		}
 	}
 	}
