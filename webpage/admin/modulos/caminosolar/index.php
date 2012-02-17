9349b4515f472c97fb58642c4e16b19aa3b8e3c6
Minoru Nomura <kaos_93@hotmail.com>
2012-02-17 14:41:24 -0800

    genera camino solar con respecto a fechas seleccionadas.

---
+++ webpage/admin/modulos/caminosolar/index.php
@@ -0,0 +1,38 @@
+<div class="prefix_1 grid_15"><h2>Camino Solar</h2></div>
+<div class="prefix_1 grid_14 suffix_1">
+<table cellpadding="0" cellspacing="0" border="0" id="activar_proveedores">
+	<thead>
+  	<tr>
+    	<td id="izq">id</td>
+      <td>Tiempo</td>
+      <td>Azimuth</td>
+      <td>Altura</td>
+      <td>Int Cero</td>
+      <td id="der">Int Uno</td>
+    </tr>
+    </thead>
+    <?php
+		$i = 0;
+		$query = mysql_query("SELECT * FROM ce_camino_solar_32t ORDER BY id DESC LIMIT 500");
+		while($row = mysql_fetch_array($query)){
+			if($i%2==0){
+				$clase = 'par';
+			}else{
+				$clase = 'non';
+			}
+			echo '<tr class="'.$clase.'">';
+			echo '<td>'.$row['id'].'</td>';
+			echo '<td>'.$row['tiempo'].'</td>';
+			echo '<td>'.$row['az'].'</td>';
+			echo '<td>'.$row['alt'].'</td>';
+			echo '<td>'.$row['intcero'].'</td>';
+			echo '<td>'.$row['intuno'].'</td>';
+			echo '</tr>';
+			$i++;
+		}
+		?>
+  </thead>
+  
+  
+</table>
+</div>
