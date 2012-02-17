1adff842056e0415ef614894c720d4f4176f416c
Minoru Nomura <kaos_93@hotmail.com>
2012-02-17 14:30:41 -0800

    se le agrego codigo a camino solar para ser ejecutado automaticamente

---
+++ webpage/system.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Documento sin título</title>
+</head>
+
+<body>
+<?php
+echo '<pre>';
+
+// Outputs all the result of shellcommand "ls", and returns
+// the last output line into $last_line. Stores the return value
+// of the shell command in $retval.
+$last_line = system('python caminoSolar.py 51', $retval);
+//$last_line = system('ls -l', $retval);	
+//$last_line = system('mkdir kk', $retval);
+
+// Printing additional info
+echo '
+</pre>
+<hr />Last line of the output: ' . $last_line . '
+<hr />Return value: ' . $retval;
+?>
+
+</body>
+</html>
