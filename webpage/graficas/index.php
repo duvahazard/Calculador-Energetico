829070673e98ec6befb8e29ee15b1199749dfc3e
Minoru Nomura <kaos_93@hotmail.com>
2012-02-17 14:31:43 -0800

    se integro este archivo en el modulo de camino solar de terrenos.

--- webpage/graficas/index.php
+++ webpage/graficas/index.php
@@ -1,3 +1,6 @@
+<?php
+require("../conexion.php");
+?>
 <html>
 <head>
 <style>
@@ -20,54 +23,28 @@
 
     // Create the data table.
     var data = new google.visualization.DataTable();
-        data.addColumn('string', 'Month');
-        data.addColumn('number', 'Cumulativo de CFE');
-        data.addColumn('number', 'Cumulativo para simular');
-        data.addRows(13);
-				data.setValue(0, 0, 'Dic');
-        data.setValue(0, 1, 0);
-        data.setValue(0, 2, 806.50);
-        data.setValue(1, 0, 'Ene');
-        data.setValue(1, 1, 1613.00);
-        data.setValue(1, 2, 1613.00);
-        data.setValue(2, 0, 'Feb');
-        data.setValue(2, 1, 1613.00);
-        data.setValue(2, 2, 2400.25);
-        data.setValue(3, 0, 'Mar');
-        data.setValue(3, 1, 3187.50);
-        data.setValue(3, 2, 3187.50);
-        data.setValue(4, 0, 'Abr');
-        data.setValue(4, 1, 3187.50);
-        data.setValue(4, 2, 4134.75);
-   		  data.setValue(5, 0, 'May');
-        data.setValue(5, 1, 5082.00);
-        data.setValue(5, 2, 5082.00);
- 			  data.setValue(6, 0, 'Jun');
-        data.setValue(6, 1, 5082.00);
-        data.setValue(6, 2, 6083.00);
-				data.setValue(7, 0, 'Jul');
-        data.setValue(7, 1, 7084.00);
-        data.setValue(7, 2, 7084.00);
-				data.setValue(8, 0, 'Ago');
-        data.setValue(8, 1, 7084.00);
-        data.setValue(8, 2, 8191.00);
-				data.setValue(9, 0, 'Sep');
-        data.setValue(9, 1, 9298.00);
-        data.setValue(9, 2, 9298.00);
-				data.setValue(10, 0, 'Oct');
-        data.setValue(10, 1, 9298.00);
-        data.setValue(10, 2, 10275.25);
-				data.setValue(11, 0, 'Nov');
-        data.setValue(11, 1, 11253.00);
-        data.setValue(11, 2, 11253.00);
-				data.setValue(12, 0, 'Dic');
-        data.setValue(12, 1, 11253.00);
-        data.setValue(12, 2, 12059.50);			
-
-
+        data.addColumn('string', 'date');
+        data.addColumn('number', 'Intcero');
+        <?php
+					extract(mysql_fetch_array(mysql_query("SELECT COUNT(*) AS cuantos FROM ce_camino_solar_48t WHERE intcero !=0")));					
+				?>
+        data.addRows(<?php echo $cuantos;?>);
+				<?php
+					$row = mysql_query("SELECT tiempo AS fecha, intcero FROM ce_camino_solar_48t WHERE intcero !=0");			
+					$i=0;
+					while($linea = mysql_fetch_array($row)){
+						$date = explode(" ", $linea['fecha']);
+						echo "data.setValue(".$i.", 0, '".$date[0]."');"."\r";
+        		echo "data.setValue(".$i.", 1, ".$linea['intcero'].");"."\r";
+						$i++;
+					}
+				?>
+				
+				
     // Set chart options
-    var options = {'title':'Consumo Cumulativo',
-                   'width':820,
+    var options = {
+									 'title':'Camino Solar',
+                   'width':940,
                    'height':500,
 									 'backgroundColor':'#FFF',
 									 'pointSize': 7
@@ -81,6 +58,6 @@
 </head>
 
 <body>
-  <div id="grafica"></div>
+	<div id="grafica"></div>
 </body>
 </html>
