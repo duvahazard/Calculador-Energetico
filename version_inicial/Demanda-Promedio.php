baf18aaa93c7b68359a812e48b6cc73bfed0887f
sunnycanuck <sunnycanuck@gmail.com>
2012-03-08 14:44:09 -0800

    very general, needs work still

---
+++ version_inicial/Demanda-Promedio.php
@@ -0,0 +1,17 @@
+If recibo every 2 months
+The value is for the month marked and the month before 
+Mes=1;
+While( Mes<=12){
+Hours=;//hours from that month
+If() {//no sample at this month exists
+  Mes=Mes+1;
+  Hours=Hours+ ;// hours of that month
+}
+Else{
+  Hours=Hours+ ;//hours from the month previous
+}
+EngAvg=the average consumption for each month included
+ConsumoProm=EngAvg/Hours;
+// save this value for the Mes actual and the Mes anterior 
+Mes=Mes+1;
+}
