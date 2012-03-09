177b3ef81713385998a8e522db6f7b5e5382039f
Minoru Nomura <kaos_93@hotmail.com>
2012-03-09 12:52:09 -0800

    se quitaron las funciones de titulo y mensajes.

--- webpage/modulos.php
+++ webpage/modulos.php
@@ -41,23 +41,4 @@ function modulos(){
 		}// switch
 	}
 }
-
-function titulo(){
-	switch($_REQUEST['mod']){
-		case 1: echo "Calculador Energ&eacute;tico";break;
-		case 2: echo "Acerca de";break;
-		case 3: echo "Proveedores";break;
-		case 4: echo "Terrenos";break;
-		case 5: echo "Recibos";break;
-		case 6: echo "Casos";break;
-		default: echo "Calculador Energ&eacute;tico";break;
-	}
-}
-
-function javascripts(){
-	switch($_REQUEST['mod']){
-		case 3: require("js/proveedores.js");break;
-		case 4: require("js/terrenos.js");break;
-	}
-}
 ?>
