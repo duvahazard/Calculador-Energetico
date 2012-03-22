df9c96ed2a4208187d526e89355aa2fc1b93932c
Minoru Nomura <kaos_93@hotmail.com>
2012-03-22 13:21:44 -0700

    se agregao una variable al url de retorno.

--- webpage/funciones.php
+++ webpage/funciones.php
@@ -7,15 +7,14 @@ session_start();
 
 //INCLUSION DE FUNCIONES
 require("functions.php");
-$_REQUEST['act'] = 1;
 
 if(!empty($_REQUEST['mod'])){
   switch($_REQUEST['mod']){
    case 1:{ 
 	 	require("fotovolrespuesta.php");
-		generaRespuestaDispositivos($idterreno, $idcaso);		
+		generaRespuestaDispositivos($idterreno, $idcaso);
+		$url = "index.php?mod=6&act=2&terreno=".$_REQUEST['terreno']."&table=".$_REQUEST['table']."&tid=".$_REQUEST['tid'];
 		
-	 	$url = "index.php?mod=6&act=2&terreno=".$_REQUEST['terreno']."&table=".$_REQUEST['table'];
 	 }break;
   }
 if(empty($url) and $url == "")
