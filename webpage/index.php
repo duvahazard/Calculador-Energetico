25941b742eeaebf26ada3f96d11bc2dcf8d353bb
Fernando Parra A. <duvahazard@gmail.com>
2013-03-08 23:42:27 -0800

    Código para agregar popups de ayuda
    Además de agregar números a las opciones principales de los pasos del
    menú

--- webpage/index.php
+++ webpage/index.php
@@ -134,6 +134,56 @@ javascripts();
 	});<?php */?>
 
 </script>
+<!-- QTIP -->
+<script type="text/javascript" src="js/jquery.qtip.min.js"></script>
+<link type="text/css" rel="stylesheet" href="css/jquery.qtip.css" />
+<script type="text/javascript">
+$(document).ready(function()
+{
+	// Make sure to only match links to wikipedia with a rel tag
+	$('a.tips[rel]').each(function()
+	{
+		// We make use of the .each() loop to gain access to each element via the "this" keyword...
+		$(this).qtip(
+		{
+			content: {
+				// Set the text to an image HTML string with the correct src URL to the loading image you want to use
+				text: '<img class="throbber" src="http://craigsworks.com/projects/qtip/images/throbber.gif" alt="Loading..." />',
+				ajax: {
+					url: $(this).attr('rel') // Use the rel attribute of each element for the url to load
+				},
+				title: {
+					//text: 'Titulo de : ' + $(this).text(), // Give the tooltip a title using each elements text
+					button: false
+				}
+			},
+			position: {
+				at: 'bottom center', // Position the tooltip above the link
+				my: 'top center',
+				viewport: $(window), // Keep the tooltip on-screen at all times
+				effect: false // Disable positioning animation
+			},
+			show: {
+				<?php if($_REQUEST['mod']!=1){ ?>event: 'click', // Don't specify a show event...<?php } ?>
+				ready: true // ... but show the tooltip when ready
+			},
+			hide:{
+				//fixed: true,
+        event: 'unfocus'
+			},
+			style: {
+				classes: 'qtip-wiki qtip-plain qtip-shadow'
+			}
+		})
+	})
+ 
+	// Make sure it doesn't follow the link when we click it
+	.click(function(event) { event.preventDefault(); });
+});
+</script>
+
+<!-- QTIP -->
+
 <style>
 .ui-datepicker-calendar{
 	display: none;
@@ -150,61 +200,71 @@ if($_REQUEST['mod'] == 4){
 <link rel="stylesheet" href="css/styles.css" />
 </head>
 <body>
-<div class="container_16">
+
 <!----------------------------- LOGOS ----------------------------->
 	<?php
 			if(isset($_SESSION['user'])){ ?>
-  <div>
-  	<div class="grid_4 alpha">
-    	&nbsp;
-    </div>
-    <div class="grid_8">
-        <div class="prefix_1 grid_6 suffix_1" id="user_field">
-          <p>Bienvenido(a): <img src="images/user.png" border="0" width="16" /> <?php echo $_SESSION['user']; ?></p>
-          <?php if($_SESSION['tipo']==1){ ?>
-					<p style="margin-top:0;"><a href="admin" target="_blank"><img src="images/engrane.png" border="0" /> Administrar Calculador</a></p>
-					<?php } ?>
-        </div><!-- user_field -->
-    </div><!-- grid_7 -->
-    <div class="grid_4 omega">
-    	&nbsp;
-    </div><!-- grid_4 -->
-  </div><!-- logos -->
+  <div id="user_field">
+	<div class="container_16">
+    <div>
+      <div class="grid_4 alpha">
+        &nbsp;
+      </div>
+      <div class="grid_8">
+          <div class="prefix_1 grid_6 suffix_1">
+            <p>Bienvenido(a): <img src="images/user.png" border="0" width="16" /> <?php echo $_SESSION['user']; ?></p>
+            <?php if($_SESSION['tipo']==1){ ?>
+            <p style="margin-top:0;"><a href="admin" target="_blank"><img src="images/engrane.png" border="0" /> Administrar Calculador</a></p>
+            <?php } ?>
+          </div><!-- user_field -->
+      </div><!-- grid_7 -->
+      <div class="grid_4 omega">
+        &nbsp;
+      </div><!-- grid_4 -->
+    </div><!-- logos -->
+  </div><!-- .container_16 -->
   <div class="clear"></div>
+</div><!-- #user-field -->
   <?php
 		}
 	?>
 <!----------------------------- LOGOS ----------------------------->
 <!----------------------------- MENU ------------------------------>
+
   <div id="menu">
-  	<div class="grid_4 alpha">
-    	<a href="index.php"><img src="images/btn_home.png" border="0" /></a>
-      <img src="images/btn_spacer.png" class="spacer_btn" border="0" />
-      <a href="index.php?mod=2"><img src="images/btn_acerca.png" border="0" /></a>
-    </div><!-- grid_4 -->
-    <div id="titulo_pagina" class="grid_8">
-    	<h1><?php titulo(); ?></h1>
-    </div><!-- titulo_pagina -->
-    <div class="grid_4 omega">
-    	<a class="login" href="actions.php?id=1"><img src="images/btn_cuenta.png" border="0" /></a>
-      <img src="images/btn_spacer.png" class="spacer_btn" border="0" />
-      <?php if(empty($_SESSION['log'])){ ?>
-      <a class="signin" href="actions.php?id=2"><img src="images/btn_registrarse.png" border="0" /></a>
-      <?php }else{ ?>
-      <a href="logout.php"><img src="images/btn_logout.png" border="0" /></a>
-      <?php } ?>
-    </div><!-- grid_4 -->
+  	<div class="container_16">
+    	<div class="grid_4 alpha">
+        <a href="index.php"><img src="images/btn_home.png" border="0" /></a>
+        <img src="images/btn_spacer.png" class="spacer_btn" border="0" />
+        <a href="index.php?mod=2"><img src="images/btn_acerca.png" border="0" /></a>
+      </div><!-- grid_4 -->
+      <div id="titulo_pagina" class="grid_8">
+        <h1><?php titulo(); ?></h1>
+      </div><!-- titulo_pagina -->
+      <div class="grid_4 omega">
+        <a class="login" href="actions.php?id=1"><img src="images/btn_cuenta.png" border="0" /></a>
+        <img src="images/btn_spacer.png" class="spacer_btn" border="0" />
+        <?php if(empty($_SESSION['log'])){ ?>
+        <a class="signin" href="actions.php?id=2"><img src="images/btn_registrarse.png" border="0" /></a>
+        <?php }else{ ?>
+        <a href="logout.php"><img src="images/btn_logout.png" border="0" /></a>
+        <?php } ?>
+      </div><!-- grid_4 -->
+    </div><!-- container_16 -->
   </div><!-- menu -->
 <!----------------------------- MENU ------------------------------>
 	<div class="spacer_2"></div>
+	<div class="clear"></div>
+
+<div class="container_16">
 <!------------------------ MENU SECUNDARIO ------------------------>
 	<?php if(!empty($_SESSION['log'])){ ?>
-  <div id="menu_secundario" align="center">
+  <div id="menu_secundario">
   	<ul>
-    	<li><a href="index.php?mod=3"><img src="images/proveedor.png" border="0" /> Proveedores</a></li>
-      <li><a href="index.php?mod=4"><img src="images/proveedor.png" border="0" /> Terrenos</a></li>
-      <li><a href="index.php?mod=5"><img src="images/proveedor.png" border="0" /> Recibos</a></li>
-      <li><a href="index.php?mod=6"><img src="images/proveedor.png" border="0" /> Casos</a></li>
+    	<li><a class="tips" href="index.php?mod=3"><span class="menu-secundario-pasos">1</span> Proveedores</a> <a class="tips" rel="qtip_files/burrowingowl.html"><img class="info-qtip-img" src="images/info.png" /></a></li>
+      <li><a class="tips" href="index.php?mod=4"><span class="menu-secundario-pasos">2</span> Terrenos</a> <a class="tips" rel="qtip_files/burrowingowl.html"><img class="info-qtip-img" src="images/info.png" /></a></li>
+      <li><a class="tips" href="index.php?mod=5"><span class="menu-secundario-pasos">3</span> Recibos</a> <a class="tips" rel="qtip_files/burrowingowl.html"><img class="info-qtip-img" src="images/info.png" /></a></li>
+      <li><a class="tips" href="index.php?mod=6"><span class="menu-secundario-pasos">4</span> Casos</a> <a class="tips" rel="qtip_files/burrowingowl.html"><img class="info-qtip-img" src="images/info.png" /></a></li>
     </ul>
     <?php }else{
     	if(isset($_REQUEST['ide'])){
@@ -229,7 +289,7 @@ if($_REQUEST['mod'] == 4){
 		  <div class="grid_16">
 			<div class="spacer_10"></div>
 			<div style="margin: 0px; display: inline; float: left;">
-				<object width="625"><param name="movie" value="http://www.youtube.com/v/JI9qGPjVHc0&hl=en&fs=1&autoplay=1"></param><param name="allowFullScreen" value="true"></param><embed src="http://www.youtube.com/v/JI9qGPjVHc0&hl=en&fs=1&autoplay=1" type="application/x-shockwave-flash" allowfullscreen="true" width="625" height="344"></embed></object>
+				<object width="625"><param name="movie" value="http://www.youtube.com/v/JI9qGPjVHc0&hl=en&fs=1"></param><param name="allowFullScreen" value="true"></param><embed src="http://www.youtube.com/v/JI9qGPjVHc0&hl=en&fs=1" type="application/x-shockwave-flash" allowfullscreen="true" width="625" height="344"></embed></object>
 			</div>
 			<div style="margin: 0px; display: inline;" align="center"><h1>Tutorial para usuario normal del calculador energ&eacute;tico</h1>
 			</div>
